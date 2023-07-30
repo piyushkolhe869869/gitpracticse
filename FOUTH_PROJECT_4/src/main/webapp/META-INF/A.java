e3f85475b0066a6edb802d586831fd91433221be
piyushkolhe869869 <kolhepiyush222@gmail.com>
2023-07-30 22:19:42 +0530

    i work on  main

--- FOUTH_PROJECT_4/src/main/webapp/META-INF/A.java
+++ FOUTH_PROJECT_4/src/main/webapp/META-INF/A.java
@@ -3,5 +3,9 @@ public class A
 	public static void main(String args[])
 	{
 		System.out.println("Hello from Ankit");
+
+		System.out.println("hello piyush");
+
+		System.out.println("hello nitin");
 	}
 }
